a5d05ca691afba1d4f92b81f64e2b65780f0fa77
Samuel Melrose <sam.melrose@a1comms.com>
2018-11-07 15:47:15 +0000

    File Logging

--- src/A1comms/GaeSupportLaravel/Log/Logger.php
+++ src/A1comms/GaeSupportLaravel/Log/Logger.php
@@ -3,7 +3,9 @@
 namespace A1comms\GaeSupportLaravel\Log;
 
 use Google\Cloud\Logging\LoggingClient;
+use Monolog\Logger;
 use Monolog\Handler\PsrHandler;
+use Monolog\Handler\StreamHandler;
 use Monolog\Handler\ErrorLogHandler;
 
 class Logger
@@ -33,7 +35,7 @@ class Logger
             // Just log here using PHP, so it goes via stderr, until structured logging
             // via /var/log files is available.
             $app->configureMonologUsing(function ($monolog) {
-                $monolog->pushHandler(new ErrorLogHandler());
+                $monolog->pushHandler(new StreamHandler('/var/log/app.log', Logger::INFO));
             });
         }
     }

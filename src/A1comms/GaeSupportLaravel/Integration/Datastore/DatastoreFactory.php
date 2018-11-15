4c5c6371f55939b48c48b5735e13d5b9b81ab414
Samuel Melrose <sam.melrose@a1comms.com>
2018-11-15 09:22:01 +0000

    Datastore GRPC

--- src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
+++ src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
@@ -8,6 +8,6 @@ class DatastoreFactory
 {
     public static function make()
     {
-        return new GDS\Gateway\RESTv1(gae_project(), null);
+        return new GDS\Gateway\GRPCv1(gae_project(), null);
     }
 }

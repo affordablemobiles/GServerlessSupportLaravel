677325c3132e9fdd78c76f4f7f0da0905224db4a
Samuel Melrose <sam.melrose@a1comms.com>
2018-11-16 12:14:56 +0000

    Revert "back to REST"
    This reverts commit edfc87fc9297cbca5e83a5a3f66cf604f8a32086.

--- src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
+++ src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
@@ -6,18 +6,8 @@ use GDS;
 
 class DatastoreFactory
 {
-    public static function make($namespace = null, $transport = 'grpc')
+    public static function make()
     {
-        switch ($transport) {
-            case 'grpc':
-                return new GDS\Gateway\GRPCv1(gae_project(), $namespace);
-                break;
-            case 'rest':
-                return new GDS\Gateway\RESTv1(gae_project(), $namespace);
-                break;
-            default:
-                throw new \Exception("Invalid Datastore Transport: " . $transport);
-                break;
-        }
+        return new GDS\Gateway\GRPCv1(gae_project(), null);
     }
 }

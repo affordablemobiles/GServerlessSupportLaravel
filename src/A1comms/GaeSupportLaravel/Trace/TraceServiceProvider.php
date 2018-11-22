6b3706e917ed2ab2a8b360946061a13bf8078bc8
Samuel Melrose <sam.melrose@a1comms.com>
2018-11-22 16:07:27 +0000

    Trace Defined Check

--- src/A1comms/GaeSupportLaravel/Trace/TraceServiceProvider.php
+++ src/A1comms/GaeSupportLaravel/Trace/TraceServiceProvider.php
@@ -34,7 +34,9 @@ class TraceServiceProvider extends ServiceProvider
         //       to help it merge properly in the tree view.
         //       Need to wait for rootSpan visibility to be changed to public.
         //       https://github.com/census-instrumentation/opencensus-php/issues/199
-        Tracer::inSpan(['name' => 'laravel/bootstrap', 'startTime' => LARAVEL_START], function () {});
+        if (defined('LARAVEL_START')) {
+            Tracer::inSpan(['name' => 'laravel/bootstrap', 'startTime' => LARAVEL_START], function () {});
+        }
 
         foreach (config('gaesupport.trace_providers', []) as $p) {
             $p::load();

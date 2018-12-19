60e4e3ba19bef5375f6b08e6a5fdea0d6b9b544b
Samuel Melrose <sam.melrose@a1comms.com>
2018-12-19 09:22:40 +0000

    php-gds update

--- src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
+++ src/A1comms/GaeSupportLaravel/Integration/Datastore/DatastoreFactory.php
@@ -6,8 +6,8 @@ use GDS;
 
 class DatastoreFactory
 {
-    public static function make()
+    public static function make($namespace = null)
     {
-        return new GDS\Gateway\GRPCv1(gae_project(), null);
+        return new GDS\Gateway\GRPCv1(gae_project(), $namespace);
     }
 }

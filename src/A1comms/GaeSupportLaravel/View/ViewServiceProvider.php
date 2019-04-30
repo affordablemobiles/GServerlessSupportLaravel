fafa4ced4b21f049b3189f9ecec8f2837bc04cd9
Samuel Melrose <sam.melrose@a1comms.com>
2019-04-30 10:32:09 +0000

    Blade Pre-Compiler: Only when APP_ENV = production

--- src/A1comms/GaeSupportLaravel/View/ViewServiceProvider.php
+++ src/A1comms/GaeSupportLaravel/View/ViewServiceProvider.php
@@ -17,7 +17,7 @@ class ViewServiceProvider extends LaravelViewServiceProvider
      */
     public function register()
     {
-        if (is_gae()){
+        if (is_gae() && (env('APP_ENV', 'production') == 'production')){
             $this->registerFactory();
 
             $this->registerGaeViewFinder();

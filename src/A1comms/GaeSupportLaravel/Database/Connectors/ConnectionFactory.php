aa2bacffd857308f0c56e4122979042c3392b3f1
Samuel Melrose <sam.melrose@a1comms.com>
2019-06-21 11:35:29 +0000

    Database Service Provider

---
+++ src/A1comms/GaeSupportLaravel/Database/Connectors/ConnectionFactory.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace A1comms\GaeSupportLaravel\Database\Connectors;
+
+use PDOException;
+use Illuminate\Support\Arr;
+use InvalidArgumentException;
+use Illuminate\Contracts\Debug\ExceptionHandler;
+use Illuminate\Database\Connectors\ConnectionFactory as LaravelConnectionFactory;
+
+class ConnectionFactory extends LaravelConnectionFactory
+{
+    /**
+     * Create a new Closure that resolves to a PDO instance.
+     *
+     * @param  array  $config
+     * @return \Closure
+     */
+    protected function createPdoResolver(array $config)
+    {
+        if ( array_key_exists('unix_socket', $config) ) {
+            return $this->createPdoResolverWithSockets($config);
+        } else if ( array_key_exists('host', $config) ) {
+            return $this->createPdoResolverWithHosts($config);
+        } else {
+            return $this->createPdoResolverWithoutHosts($config);
+        }
+    }
+
+    /**
+     * Create a new Closure that resolves to a PDO instance with a specific socket or an array of sockets.
+     *
+     * @param  array  $config
+     * @return \Closure
+     */
+    protected function createPdoResolverWithSockets(array $config)
+    {
+        return function () use ($config) {
+            foreach (Arr::shuffle($sockets = $this->parseSockets($config)) as $key => $socket) {
+                $config['unix_socket'] = $socket;
+
+                \Log::info('Connecting to DB unix_socket: ' . $socket);
+
+                try {
+                    return $this->createConnector($config)->connect($config);
+                } catch (PDOException $e) {
+                    if (count($hosts) - 1 === $key && $this->container->bound(ExceptionHandler::class)) {
+                        $this->container->make(ExceptionHandler::class)->report($e);
+                    }
+                }
+            }
+
+            throw $e;
+        };
+    }
+
+    /**
+     * Parse the hosts configuration item into an array.
+     *
+     * @param  array  $config
+     * @return array
+     */
+    protected function parseSockets(array $config)
+    {
+        $sockets = Arr::wrap($config['unix_socket']);
+
+        if (empty($sockets)) {
+            throw new InvalidArgumentException('Database unix_socket array is empty.');
+        }
+
+        return $sockets;
+    }
+}
---
+++ src/A1comms/GaeSupportLaravel/Database/DatabaseServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace A1comms\GaeSupportLaravel\Database;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Database\DatabaseServiceProvider as LaravelDatabaseServiceProvider;
+use A1comms\GaeSupportLaravel\Database\Connectors\ConnectionFactory;
+
+class DatabaseServiceProvider extends LaravelDatabaseServiceProvider
+{
+    /**
+     * Register the primary database bindings.
+     *
+     * @return void
+     */
+    protected function registerConnectionServices()
+    {
+        // The connection factory is used to create the actual connection instances on
+        // the database. We will inject the factory into the manager so that it may
+        // make the connections while they are actually needed and not of before.
+        $this->app->singleton('db.factory', function ($app) {
+            return new ConnectionFactory($app);
+        });
+
+        // The database manager is used to resolve various connections, since multiple
+        // connections might be managed. It also implements the connection resolver
+        // interface which may be used by other components requiring connections.
+        $this->app->singleton('db', function ($app) {
+            return new DatabaseManager($app, $app['db.factory']);
+        });
+
+        $this->app->bind('db.connection', function ($app) {
+            return $app['db']->connection();
+        });
+    }
+}

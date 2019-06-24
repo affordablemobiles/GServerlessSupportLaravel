066aea6ae5b30820785d566352622395f4e9921a
Samuel Melrose <sam.melrose@a1comms.com>
2019-06-24 10:39:42 +0000

    Cloudflare Integration

---
+++ src/A1comms/GaeSupportLaravel/Integration/Cloudflare/update_ips_cli.php
@@ -0,0 +1,26 @@
+<?php
+
+define('CF_V4_IPS_URL', 'https://www.cloudflare.com/ips-v4');
+define('CF_V6_IPS_URL', 'https://www.cloudflare.com/ips-v6');
+
+$ipv4_list  = file_get_contents(CF_V4_IPS_URL);
+$ipv4_arr   = explode("\n", $ipv4_list);
+
+$ipv6_list  = file_get_contents(CF_V6_IPS_URL);
+$ipv6_arr   = explode("\n", $ipv6_list);
+
+writeFile(__DIR__.'/cf-ipv4.php', remove_empty($ipv4_arr));
+writeFile(__DIR__.'/cf-ipv6.php', remove_empty($ipv6_arr));
+
+function remove_empty($array) {
+    $array = array_map('trim', $array);
+    return array_filter($array, '_remove_empty_internal');
+}
+
+function _remove_empty_internal($value) {
+    return !empty($value) || $value === 0;
+}
+
+function writeFile($file, $array) {
+    file_put_contents($file, '<?php return '.var_export($array, true).';'.PHP_EOL);
+}

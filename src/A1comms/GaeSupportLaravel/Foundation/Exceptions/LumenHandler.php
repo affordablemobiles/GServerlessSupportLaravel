b6083a60b259b3960cf7407f73c8f72ddd43cbd4
Samuel <sam.melrose@a1comms.com>
2019-08-05 15:58:54 +0100

    Lumen Compatibility: Exception Handler

---
+++ src/A1comms/GaeSupportLaravel/Foundation/Exceptions/LumenHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace A1comms\GaeSupportLaravel\Foundation\Exceptions;
+
+use Exception;
+use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
+use A1comms\GaeSupportLaravel\Integration\ErrorReporting\Report as ErrorBootstrap;
+
+class LumenHandler extends ExceptionHandler
+{
+    /**
+     * Report or log an exception.
+     *
+     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    public function report(Exception $exception)
+    {
+        parent::report($exception);
+
+        // Log the error out to Stackdriver Error Reporting.
+        if ($this->shouldReport($exception)) {
+            try {
+                ErrorBootstrap::exceptionHandler($exception);
+            } catch (Exception $ex) {
+
+            }
+        }
+    }
+}

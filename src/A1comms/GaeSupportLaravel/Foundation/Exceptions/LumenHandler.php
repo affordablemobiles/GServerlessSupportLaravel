ccafb89e2f1a069ce1fed4f40b82c178a46a4b14
Samuel Melrose <sam.melrose@a1comms.com>
2020-07-23 11:44:01 +0100

    Laravel 6 was broken with: PHP Fatal error:  Declaration of A1comms\GaeSupportLaravel\Foundation\Exceptions\Handler::report(Throwable $exception) must be compatible with Illuminate\Foundation\Exceptions\Handler::report(Exception $e)

--- src/A1comms/GaeSupportLaravel/Foundation/Exceptions/LumenHandler.php
+++ src/A1comms/GaeSupportLaravel/Foundation/Exceptions/LumenHandler.php
@@ -2,7 +2,7 @@
 
 namespace A1comms\GaeSupportLaravel\Foundation\Exceptions;
 
-use Throwable;
+use Exception;
 use Laravel\Lumen\Exceptions\Handler as ExceptionHandler;
 use A1comms\GaeSupportLaravel\Integration\ErrorReporting\Report as ErrorBootstrap;
 
@@ -13,10 +13,10 @@ class LumenHandler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Throwable  $exception
+     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Throwable $exception)
+    public function report(Exception $exception)
     {
         parent::report($exception);
 

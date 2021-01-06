adda396564882754b4e456db6296b0602543bca0
Samuel Melrose <sam.melrose@a1comms.com>
2021-01-06 12:29:05 +0000

    Cloud Function for clearing sessions

---
+++ cloud-functions/function.go
@@ -0,0 +1,91 @@
+package function
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/datastore"
+	"google.golang.org/api/iterator"
+	"maze.io/x/duration"
+)
+
+// HandleSessionCleanRequest handles garbage collections of Datastore sessions.
+func HandleSessionCleanRequest(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	sessionDuration, err := duration.ParseDuration(os.Getenv("SESSION_DURATION"))
+	if err != nil {
+		panic(err)
+	}
+
+	oldestTime := time.Now().Add(time.Duration(sessionDuration) * -1)
+
+	client, err := datastore.NewClient(ctx, "DetectProjectID")
+	if err != nil {
+		panic(err)
+	}
+
+	query := datastore.NewQuery("sessions").
+		Filter("lastaccess <", oldestTime).
+		KeysOnly()
+
+	keyChannel := make(chan *datastore.Key)
+
+	jobs := 0
+	doneChan := make(chan error)
+
+	jobs++
+	go func(done chan<- error, keys <-chan *datastore.Key) {
+		kArr := []*datastore.Key{}
+
+		for k := range keys {
+			kArr = append(kArr, k)
+
+			if len(keys) >= 100 {
+				err := client.DeleteMulti(ctx, kArr)
+				if err != nil {
+					done <- err
+					return
+				}
+
+				kArr = []*datastore.Key{}
+			}
+		}
+
+		err := client.DeleteMulti(ctx, kArr)
+		if err != nil {
+			done <- err
+			return
+		}
+
+		done <- nil
+	}(doneChan, keyChannel)
+
+	jobs++
+	go func(done chan<- error, keys chan<- *datastore.Key) {
+		for t := client.Run(ctx, query); ; {
+			k, err := t.Next(nil)
+			if err == iterator.Done {
+				break
+			}
+			if err != nil {
+				done <- err
+				return
+			}
+
+			keys <- k
+		}
+
+		done <- nil
+	}(doneChan, keyChannel)
+
+	for i := 0; i < jobs; i++ {
+		err := <-doneChan
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	http.Error(w, "OK", 200)
+}
---
+++ cloud-functions/go.mod
@@ -0,0 +1,9 @@
+module github.com/a1comms/GaeSupportLaravel/cloud-functions
+
+go 1.15
+
+require (
+	cloud.google.com/go/datastore v1.3.0
+	google.golang.org/api v0.36.0
+	maze.io/x/duration v0.0.0-20160924141736-faac084b6075
+)

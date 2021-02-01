c6805fc8d6e2ad941be4a4f38fda5129777e7304
Samuel Melrose <sam.melrose@a1comms.com>
2021-02-01 19:27:27 +0000

    Revert "Laravel 7 Compat: Backwards Compatible: Throwable instead of Exception"
    This reverts commit 4bbb1f6cbbc2073ee1e8db886b6e44579daa16ee.
    
    It was throwing errors on Laravel 6.0, which is our target, because the framework explicity specifies Exception not Throwable, so it isn't backwards compatible.

--- src/A1comms/GaeSupportLaravel/Foundation/Exceptions/Handler.php
+++ src/A1comms/GaeSupportLaravel/Foundation/Exceptions/Handler.php
@@ -2,7 +2,7 @@
 
 namespace A1comms\GaeSupportLaravel\Foundation\Exceptions;
 
-use Throwable;
+use Exception;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use A1comms\GaeSupportLaravel\Integration\ErrorReporting\Report as ErrorBootstrap;
 
@@ -13,20 +13,20 @@ class Handler extends ExceptionHandler
      *
      * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
      *
-     * @param  \Throwable  $exception
+     * @param  \Exception  $exception
      * @return void
      */
-    public function report(Throwable $exception)
+    public function report(Exception $exception)
     {
         parent::report($exception);
 
         // Log the error out to Stackdriver Error Reporting.
-        if ($this->shouldReport($exception)) {
-            try {
-                ErrorBootstrap::exceptionHandler($exception);
-            } catch (Exception $ex) {
+		if ($this->shouldReport($exception)) {
+			try {
+				ErrorBootstrap::exceptionHandler($exception);
+			} catch (Exception $ex) {
 
-            }
-        }
+			}
+		}
     }
 }

b8197e68b06863e7a09c2f6f1fe522698b7c7c4e
Samuel Melrose <sam.melrose@a1comms.com>
2021-02-02 16:03:43 +0000

    fix debugger source location

--- src/scripts/entrypoint_debugger.sh
+++ src/scripts/entrypoint_debugger.sh
@@ -8,7 +8,7 @@ serve --enable-dynamic-workers public/index.php &
 WPID=$!;
 
 echo "Starting Cloud Debugger..."
-php -d auto_prepend_file='' -d disable_functions='' /srv/vendor/bin/google-cloud-debugger -s /tmp/srv &
+php -d auto_prepend_file='' -d disable_functions='' /srv/vendor/bin/google-cloud-debugger -s /workspace &
 DPID=$1;
 
 echo "Wuhoo! Environment Ready!"

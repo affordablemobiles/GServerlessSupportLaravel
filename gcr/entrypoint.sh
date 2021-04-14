e0353c7f209b254141d34f7a28ffb3a4809a97c9
Samuel Melrose <sam.melrose@a1comms.com>
2021-04-14 13:08:55 +0100

    Cloud Run: entrypoint more reliable

--- gcr/entrypoint.sh
+++ gcr/entrypoint.sh
@@ -3,7 +3,11 @@
 echo "Configuring logpipe..."
 mkfifo -m 600 /tmp/logpipe
 cat <> /tmp/logpipe 1>&2 &
-CAT=$!
+CPID=$!
 
 echo "Starting PHP runtime..."
-serve --enable-dynamic-workers public/index.php
+serve --enable-dynamic-workers public/index.php &
+SPID=$1
+
+echo "Wuhoo! Environment Ready!"
+wait -n $CPID $SPID

da46e332d52bf2713111bd72178e7d0434bb6483
Tor Turner <tor.turner@a1comms.com>
2021-04-14 12:01:45 +0100

    Updating entrypoint

--- docs/examples/lumen/buildpack/build_container_image_live.sh
+++ docs/examples/lumen/buildpack/build_container_image_live.sh
@@ -4,4 +4,4 @@ PHPBUILDER=$(tail -n 1 /workspace/phpbuilder)
 
 bash ./generate_env_live.sh
 
-pack build --builder eu.gcr.io/gae-runtimes/buildpacks/php74/builder:$PHPBUILDER --publish eu.gcr.io/$PROJECT_ID/example-service:$COMMIT_SHA --env-file ./.env
+pack build --builder eu.gcr.io/gae-runtimes/buildpacks/php74/builder:$PHPBUILDER --publish eu.gcr.io/$PROJECT_ID/example-service:$COMMIT_SHA --env=GOOGLE_ENTRYPOINT=/workspace/vendor/a1comms/gae-support-laravel/gcr/entrypoint.sh

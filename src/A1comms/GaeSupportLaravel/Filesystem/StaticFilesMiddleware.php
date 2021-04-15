dda328d1480c1e7ed37fbfbb5bcb049896585c95
Samuel Melrose <sam.melrose@a1comms.com>
2021-04-15 11:30:47 +0100

    Cloud Run: fix StaticFilesMiddleware

--- src/A1comms/GaeSupportLaravel/Filesystem/StaticFilesMiddleware.php
+++ src/A1comms/GaeSupportLaravel/Filesystem/StaticFilesMiddleware.php
@@ -2,6 +2,7 @@
 
 namespace A1comms\GaeSupportLaravel\Filesystem;
 
+use Closure;
 use Illuminate\Http\Request;
 
 class StaticFilesMiddleware
@@ -19,7 +20,13 @@ class StaticFilesMiddleware
             $path = public_path() . '/' . request()->path();
             
             if (is_file($path)) {
-                return response()->file($path);
+                $finfo = finfo_open( FILEINFO_MIME_TYPE );
+                $mtype = finfo_file( $finfo, $path );
+                finfo_close( $finfo );
+                
+                return response()->file($path, [
+                    'Content-Type' => $mtype,
+                ]);
             }
         }
 

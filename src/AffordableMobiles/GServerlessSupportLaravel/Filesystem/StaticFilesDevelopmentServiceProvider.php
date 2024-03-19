a255c17e180579470b45b2d285944c7328e4fa50
Samuel Melrose <sam.melrose@a1comms.com>
2024-03-19 16:44:54 +0000

    remove StaticFiles stuff for gcr

--- src/AffordableMobiles/GServerlessSupportLaravel/Filesystem/StaticFilesDevelopmentServiceProvider.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace AffordableMobiles\GServerlessSupportLaravel\Filesystem;
-
-class StaticFilesDevelopmentServiceProvider extends StaticFilesServiceProvider {}

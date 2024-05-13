758f7778fcd2165bb8c9702c4066e7fe8f8098b1
Samuel Melrose <sam.melrose@a1comms.com>
2024-05-13 10:56:58 +0100

    Trace: improve SQL tracing

--- src/AffordableMobiles/GServerlessSupportLaravel/Trace/Instrumentation/Laravel/Watchers/QueryWatcher.php
+++ src/AffordableMobiles/GServerlessSupportLaravel/Trace/Instrumentation/Laravel/Watchers/QueryWatcher.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace AffordableMobiles\GServerlessSupportLaravel\Trace\Instrumentation\Laravel\Watchers;
 
+use AffordableMobiles\GServerlessSupportLaravel\Trace\Instrumentation\SimpleSpan;
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Connection;
+use Illuminate\Database\Connectors\ConnectorInterface;
 use Illuminate\Support\Str;
 use OpenTelemetry\API\Instrumentation\CachedInstrumentation;
-use OpenTelemetry\API\Trace\SpanKind;
 use OpenTelemetry\SemConv\TraceAttributes;
+
+use function OpenTelemetry\Instrumentation\hook;
 
 class QueryWatcher extends Watcher
 {
@@ -20,44 +23,47 @@ class QueryWatcher extends Watcher
     /** @psalm-suppress UndefinedInterfaceMethod */
     public function register(Application $app): void
     {
-        $app['events']->listen(QueryExecuted::class, [$this, 'recordQuery']);
-    }
+        hook(
+            ConnectorInterface::class,
+            'connect',
+            pre: function (ConnectorInterface $conn, array $params, string $class, string $function, ?string $filename, ?int $lineno): void {
+                SimpleSpan::pre($this->instrumentation, 'sql CONNECT', [
+                    'driver'      => $params[0]['driver']      ?? 'unknown',
+                    'host'        => $params[0]['host']        ?? '',
+                    'unix_socket' => $params[0]['unix_socket'] ?? '',
+                ]);
+            },
+            post: static function (ConnectorInterface $conn, array $params, mixed $response, ?\Throwable $exception): void {
+                SimpleSpan::post();
+            },
+        );
 
-    /**
-     * Record a query.
-     */
-    /** @psalm-suppress UndefinedThisPropertyFetch */
-    public function recordQuery(QueryExecuted $query): void
-    {
-        $nowInNs = (int) (microtime(true) * 1E9);
+        hook(
+            Connection::class,
+            'runQueryCallback',
+            pre: function (Connection $conn, array $params, string $class, string $function, ?string $filename, ?int $lineno): void {
+                try {
+                    $sql = $params[0];
 
-        $operationName = Str::upper(Str::before($query->sql, ' '));
-        if (!\in_array($operationName, ['SELECT', 'INSERT', 'UPDATE', 'DELETE'], true)) {
-            $operationName = null;
-        }
+                    $operationName = Str::upper(Str::before($sql, ' '));
+                    if (!\in_array($operationName, ['SELECT', 'INSERT', 'UPDATE', 'DELETE'], true)) {
+                        $operationName = null;
+                    }
 
-        /** @psalm-suppress ArgumentTypeCoercion */
-        $span = $this->instrumentation->tracer()->spanBuilder('sql '.$operationName)
-            ->setSpanKind(SpanKind::KIND_CLIENT)
-            ->setStartTimestamp($this->calculateQueryStartTime($nowInNs, $query->time))
-            ->startSpan()
-        ;
-
-        $attributes = [
-            TraceAttributes::DB_SYSTEM    => $query->connection->getDriverName(),
-            TraceAttributes::DB_NAME      => $query->connection->getDatabaseName(),
-            TraceAttributes::DB_OPERATION => $operationName,
-            TraceAttributes::DB_USER      => $query->connection->getConfig('username'),
-        ];
-
-        $attributes[TraceAttributes::DB_STATEMENT] = $query->sql;
-        // @psalm-suppress PossiblyInvalidArgument
-        $span->setAttributes($attributes);
-        $span->end($nowInNs);
-    }
-
-    private function calculateQueryStartTime(int $nowInNs, float $queryTimeMs): int
-    {
-        return (int) ($nowInNs - ($queryTimeMs * 1E6));
+                    SimpleSpan::pre($this->instrumentation, 'sql '.$operationName, [
+                        TraceAttributes::DB_SYSTEM    => $conn->getDriverName(),
+                        TraceAttributes::DB_NAME      => $conn->getDatabaseName(),
+                        TraceAttributes::DB_OPERATION => $operationName,
+                        TraceAttributes::DB_USER      => $conn->getConfig('username'),
+                        TraceAttributes::DB_STATEMENT => $sql,
+                    ]);
+                } catch (\Throwable $e) {
+                    report($e);
+                }
+            },
+            post: static function (Connection $conn, array $params, mixed $response, ?\Throwable $exception): void {
+                SimpleSpan::post();
+            },
+        );
     }
 }

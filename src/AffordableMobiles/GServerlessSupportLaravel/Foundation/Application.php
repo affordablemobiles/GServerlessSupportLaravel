e64fe14ec3390f79314788474754735419c1f01c
Samuel Melrose <sam.melrose@affordablemobiles.co.uk>
2024-09-12 09:39:45 +0000

    Console: Verbose Exceptions in Cloud Build

--- src/AffordableMobiles/GServerlessSupportLaravel/Foundation/Application.php
+++ src/AffordableMobiles/GServerlessSupportLaravel/Foundation/Application.php
@@ -5,10 +5,13 @@ declare(strict_types=1);
 namespace AffordableMobiles\GServerlessSupportLaravel\Foundation;
 
 use AffordableMobiles\GServerlessSupportLaravel\Log\LogServiceProvider;
+use Illuminate\Contracts\Console\Kernel as ConsoleKernelContract;
 use Illuminate\Events\EventServiceProvider;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Log\Context\ContextServiceProvider;
 use Illuminate\Routing\RoutingServiceProvider;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\ConsoleOutput;
 
 class Application extends LaravelApplication
 {
@@ -30,6 +33,25 @@ class Application extends LaravelApplication
             ->withCommands()
             ->withProviders()
         ;
+    }
+
+    /**
+     * Handle the incoming Artisan command.
+     *
+     * @return int
+     */
+    public function handleCommand(InputInterface $input)
+    {
+        $kernel = $this->make(ConsoleKernelContract::class);
+
+        $status = $kernel->handle(
+            $input,
+            new ConsoleOutput(ConsoleOutput::VERBOSITY_VERY_VERBOSE)
+        );
+
+        $kernel->terminate($input, $status);
+
+        return $status;
     }
 
     /**

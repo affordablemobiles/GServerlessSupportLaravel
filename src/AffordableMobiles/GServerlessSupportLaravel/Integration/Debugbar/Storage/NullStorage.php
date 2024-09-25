7fa5c1fde5c6c03ff277b8d686871ed23fd898ef
Samuel Melrose <sam.melrose@affordablemobiles.co.uk>
2024-09-25 13:25:13 +0000

    DebugBar: NullStorage

---
+++ src/AffordableMobiles/GServerlessSupportLaravel/Integration/Debugbar/Storage/NullStorage.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AffordableMobiles\GServerlessSupportLaravel\Integration\Debugbar\Storage;
+
+use DebugBar\Storage\StorageInterface;
+
+class NullStorage implements StorageInterface
+{
+    /**
+     * Saves collected data.
+     *
+     * @param string $id
+     * @param string $data
+     */
+    public function save($id, $data): void {}
+
+    /**
+     * Returns collected data with the specified id.
+     *
+     * @param string $id
+     */
+    public function get($id): array
+    {
+        return [];
+    }
+
+    /**
+     * Returns a metadata about collected data.
+     *
+     * @param int $max
+     * @param int $offset
+     */
+    public function find(array $filters = [], $max = 20, $offset = 0): array
+    {
+        return [];
+    }
+
+    /**
+     * Clears all the collected data.
+     */
+    public function clear(): void {}
+}

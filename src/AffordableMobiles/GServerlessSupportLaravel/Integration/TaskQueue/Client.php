e941cc66d876426d419b3f08c5bc3e00895195b8
Samuel Melrose <sam.melrose@affordablemobiles.co.uk>
2025-02-27 11:23:04 +0000

    fix CloudTasksClient namespace

--- src/AffordableMobiles/GServerlessSupportLaravel/Integration/TaskQueue/Client.php
+++ src/AffordableMobiles/GServerlessSupportLaravel/Integration/TaskQueue/Client.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace AffordableMobiles\GServerlessSupportLaravel\Integration\TaskQueue;
 
-use Google\Cloud\Tasks\V2\CloudTasksClient;
+use Google\Cloud\Tasks\V2\Client\CloudTasksClient;
 
 class Client
 {
